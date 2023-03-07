5620cafa6e8858ab70d2722583c617d9e9833fc4
Spidw3b <jarod64@hotmail.fr>
2023-03-07 10:38:04 +0100

    exo1

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 import {} from "./jquery.js"
 
-$(document).ready(function(){
-    $("body").css("background-color","#111");
-    $("#maDiv").append("<footer></footer>");
-    $("footer").append("<p>C'est quoi ce bins ?</p>");
-    $("footer").css("color","#fff");
+// $(document).ready(function(){
+//     $("body").css("background-color","#111");
+//     $("#maDiv").append("<footer></footer>");
+//     $("footer").append("<p>C'est quoi ce bins ?</p>");
+//     $("footer").css("color","#fff");
 
+// });
+
+//EXERCICE 1
+
+let i = 0;
+$("#affichage-plus").html(i);
+$("#click-plus").click(function(){
+    i++;
+    $("#affichage-plus").html(i);
 });
+
